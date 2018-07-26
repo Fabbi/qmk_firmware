5281feccd2fbf6e4f59b6ffabf72d404e78aae8d
Fabian Schweinfurth <github@derfabbi.de>
2018-07-26 12:13:08 +0200

    added hardware layer with DFU RESET

--- keyboards/0fabbi/keymaps/fabbi/keymap.c
+++ keyboards/0fabbi/keymaps/fabbi/keymap.c
@@ -18,30 +18,40 @@ enum custom_keycodes {
 #define BASE 0 // default layer
 #define NUMBERS 1 // MOD (green) layer with numbers
 #define CONTROL 2 // controlling (blue) layer (arrow keys, F-Keys, Consumer Keys..)
+#define HARDWARE 3 // hardware layer
 
+#define ______ KC_TRNS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] =
 LAYER_PRETTY(
       KC_GRAVE,   KC_TAB,  KC_Q,      KC_W,    KC_E,  KC_R,  KC_T,     KC_Y,     KC_U,  KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC,
       MACRO12,    KC_ESC,  KC_A,      KC_S,    KC_D,  KC_F,  KC_G,     KC_H,     KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT,
-      KC_APP,  KC_GRAVE,  KC_Z,      KC_X,    KC_C,  KC_V,  KC_B,     KC_N,     KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, KC_TRNS,
-      KC_LALT,   KC_LGUI, KC_LSHIFT, KC_LCTL, TT(CONTROL),             KC_TRNS, TT(NUMBERS), KC_SPACE, TT(CONTROL), KC_RGUI
+      KC_APP,  KC_GRAVE,  KC_Z,      KC_X,    KC_C,  KC_V,  KC_B,     KC_N,     KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_BSLS, ______,
+      KC_LALT,   KC_LGUI, KC_LSHIFT, KC_LCTL, TT(CONTROL),             ______, TT(NUMBERS), KC_SPACE, TT(CONTROL), KC_RGUI
       ),
 [NUMBERS] =
 LAYER_PRETTY(
-  KC_TRNS, KC_TRNS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7, KC_8, KC_9, KC_0,  KC_MINUS, KC_EQL,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_4, KC_5, KC_6, KC_TRNS, KC_TRNS,    KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_1, KC_2, KC_3, KC_TRNS, KC_TRNS, KC_TRNS,
-                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_0, KC_DOT
+  ______, ______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,      KC_6,    KC_7, KC_8, KC_9, KC_0,  KC_MINUS, KC_EQL,
+  ______, ______, ______, ______, ______, ______, ______,   ______, KC_4, KC_5, KC_6, ______, ______,    ______,
+  ______, ______, ______, ______, ______, ______, ______,   ______, KC_1, KC_2, KC_3, ______, ______, ______,
+           ______, ______, ______, ______, TT(HARDWARE) ,   ______, ______, ______, KC_0, KC_DOT
   ),
 [CONTROL] =
 LAYER_PRETTY(
-  KC_TRNS, KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU,   KC_MPRV, KC_MPLY, KC_MNXT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_BSPC, KC_TRNS, KC_TRNS
+  ______, ______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+  ______, ______, ______, ______, ______, ______, ______,   KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, ______, ______, ______,
+  ______, ______, ______, ______, KC_MUTE, KC_VOLD, KC_VOLU,   KC_MPRV, KC_MPLY, KC_MNXT, ______, ______, ______, ______,
+  ______, ______, ______, ______, ______,   ______, TT(HARDWARE), KC_BSPC, ______, ______
+  ),
+[HARDWARE] =
+LAYER_PRETTY(
+ ______, ______, ______, ______, ______, ______, RESET, ______, ______, ______, ______, ______, ______, ______,
+ ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+ ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______,
+ ______, ______, ______, ______, ______, ______, ______, ______, ______, ______
   )
 };
+#undef _______
 
 void matrix_init_user(void)
 {
