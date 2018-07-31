cffe55850fb03f56d4f133fa5919e5c047ab1e39
Fabian Schweinfurth <github@derfabbi.de>
2018-07-31 14:38:33 +0200

    fix layer macro

--- keyboards/0fabbi/0fabbi.h
+++ keyboards/0fabbi/0fabbi.h
@@ -33,11 +33,11 @@ void fabbi_expander_init(void);
               k00,k01,k02,k03,k04,k05,k06,                                         \
               k10,k11,k12,k13,k14,k15,k16,                                         \
               k20,k21,k22,k23,k24,k25,k26,                                         \
-                              k30,k31,k32,k33,k34,                                 \
+              e01,e02,e03,e04,k30,k31,k32,k33,k34,      \
               /* right side */                                                     \
-                      k07,k08,k09,k0A,k0B,k0C,k0D,                                 \
-                      k17,k18,k19,k1A,k1B,k1C,k1D,                                 \
-                      k27,k28,k29,k2A,k2B,k2C,k2D,                                 \
+              e05,e06,k07,k08,k09,k0A,k0B,k0C,k0D,                      \
+              e07,e08,k17,k18,k19,k1A,k1B,k1C,k1D,                      \
+              e09,e10,k27,k28,k29,k2A,k2B,k2C,k2D,                      \
               k35,k36,k37,k38,k39                                                  \
               )                                                                    \
   {                                                                                \
