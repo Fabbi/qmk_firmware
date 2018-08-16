bef568f0b3f8042c55f3a27117ea55389ff15ac7
Fabian Schweinfurth <github@derfabbi.de>
2018-08-16 17:24:57 +0200

    some cleanup and preperations

--- keyboards/0fabbi/fabbi.mk
+++ keyboards/0fabbi/fabbi.mk
@@ -1,17 +1,25 @@
 FDEBUG = yes # Master of debug
 # OPT_DEFS += -DDEBUG_MATRIX_SCAN_RATE
 
-# # Display stuff..
-# OPT_DEFS += -DSSD1306OLED
+# RAW_ENABLE = yes
 
-# OPT_DEFS += -DSSD1306_DISPLAY_HEIGHT=64
-# OPT_DEFS += -DSSD1306_USE_BITMAP
+# SSD1306_ENABLE = yes
+# TRACKPOINT_ENABLE = yes
 
-# SRC += $(TOP_DIR)/drivers/avr/ssd1306.c
+# ######################
+# Display stuff..
+ifeq ($(SSD1306_ENABLE), yes)
+  OPT_DEFS += -DSSD1306OLED
+  OPT_DEFS += -DSSD1306_USE_BITMAP
+  OPT_DEFS += -DSSD1306_DISPLAY_HEIGHT=64
+  SRC += $(TOP_DIR)/drivers/avr/ssd1306.c
+endif
 
 # Trackpoint
-# PS2_MOUSE_ENABLE        = yes
-# PS2_USE_USART           = yes
+ifeq ($(TRACKPOINT_ENABLE), yes)
+  PS2_MOUSE_ENABLE        = yes
+  PS2_USE_USART           = yes
+endif
 
 # bootmagic.. dunno if needed..
-BOOTMAGIC_ENABLE				= no
+# BOOTMAGIC_ENABLE				= no
